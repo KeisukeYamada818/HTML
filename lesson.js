04a14f3222148c6bffec1c34c7bb1f2a3056049e
KeisukeYamada818 <89022879+KeisukeYamada818@users.noreply.github.com>
2021-08-28 13:35:41 +0900

    初期コミット

---
+++ lesson.js
@@ -0,0 +1,17 @@
+    var omikuji =Math.random()*100;
+    if(omikuji>=0 && omikuji<=9) {
+        alert("大吉");
+    }else if(omikuji>=10 && omikuji<=34) {
+        alert("中吉");
+    }else if(omikuji>=35 && omikuji<=64) {
+        alert("小吉");
+    }else if(omikuji>=65 && omikuji<=84) {
+        alert("吉");
+    }else if(omikuji>=85 && omikuji<=94) {
+        alert("凶");
+    }else if(omikuji>=95 && omikuji<=99) {
+        alert("大凶");
+}
+
+
+console.log('読み込み確認')
